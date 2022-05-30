c805aa6e91da0fe05274eae5938d190d71dff947
Ian Duran <ianjduran@gmail.com>
2022-05-30 11:29:36 -0500

    added heartbeat task

---
+++ Vanttec_STM_Common/Core/Inc/heartbeat_led_task.h
@@ -0,0 +1,16 @@
+/*
+ * heartbeat_led_task.h
+ *
+ * 	Created on: may 30th, 2022
+ * 		Author: Ian
+ *
+*/
+
+#ifndef INC_HEARTBEAT_LED_TASK_H_
+#define INC_HEARTBEAT_LED_TASK_H_
+
+#define HEARTBEAT_DELAY 500
+
+void heartbeat_task();
+
+#endif /*INC_HEARTBEAT_LED_TASK_H_*/
